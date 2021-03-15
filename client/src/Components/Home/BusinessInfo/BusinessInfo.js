9dc05fe1faf193d2d63e34d7380a1650db990342
Asif Mohammad <asifratul45@gmail.com>
2021-03-15 19:47:38 +0600

    commit

---
+++ client/src/Components/Home/BusinessInfo/BusinessInfo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const infoData = [
+    {
+        title : 'Opening Hours',
+        description: 'We are open 7 days',
+        icon: '',
+        background: 'primary'
+    },
+    {
+        title : 'Visit Our Location',
+        description: 'Brooklyn, NY  10003 USA',
+        icon: '',
+        background: 'dark'
+    },
+    {
+        title : 'Call us now',
+        description: '+15697854124',
+        icon: '',
+        background: 'primary'
+    }
+]
+
+const BusinessInfo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BusinessInfo;
